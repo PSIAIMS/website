15acf22e8a036e519e28d4063ab2347ec56ef3ae
msbrown123 <101326300+msbrown123@users.noreply.github.com>
2026-04-02 11:12:29 +0100

    Create first-script.R

---
+++ lunchbites/intro-ides/first-script.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+
+our_num <- 2 + 2
+ggplot(data = mtcars, mapping = aes(y = mpg, x = cyl)) +
+  geom_point()
+# This is a coment 
+
+# This is section 1 -------------------------------------------------------
+
+
+
+
+
+
+
+# This is section 2 -------------------------------------------------------
